089101a086be658abe422e5cb7e59dca3a805eb5
Chiraag Sharma <work.chiraagsharma@gmail.com>
2023-08-07 00:45:30 +0530

    Python interpreterr issue solution

--- jenkins_pipeline.groovy
+++ jenkins_pipeline.groovy
@@ -1,54 +1,54 @@
 pipeline {
     agent any
-    
+
     stages {
         stage('Build') {
             steps {
                 timestamps {
                     echo "Build started"
-                    echo "Compiling the python3 code..."
-                    sh 'python3 -m pip install Flask'
-                    sh 'python3 test_app.py'
+                    echo "Compiling the Python code..."
+                    withEnv(["PATH+PYTHON=/path/to/python/bin"]) {
+                        sh 'python -m pip install Flask'
+                        sh 'python test_app.py'
+                    }
                     echo "Build completed"
                 }
             }
         }
-        
+
         stage('Deploy') {
-    	   steps {
-        	echo "Deploying the Flask web application..."
+            environment { // Use the environment block to define the variable
+                deployDir = "$WORKSPACE/web"
+            }
+            steps {
+                echo "Deploying the Flask web application..."
+                
+                // Application Installation and Startup
+                sh 'mkdir -p $deployDir' // Use the defined variable here
+                sh 'curl -O https://bootstrap.pypa.io/get-pip.py'
+                withEnv(["PATH+PYTHON=/usr/bin/python3"]) {
+                    sh 'python get-pip.py --user'
+                    sh 'python -m pip install Flask'
+                }
 
-        	// Use the Jenkins workspace as the deployment directory
-        	def deployDir = "$WORKSPACE/web"
-        	sh "mkdir -p ${deployDir}"
+                // Start Flask Application in the Background
+                sh "nohup python $deployDir/web.py > /dev/null 2>&1 &"
 
-       		// Application Installation and Startup
-        	sh 'curl -O https://bootstrap.pypa.io/get-pip.py'
+                echo "Flask web application has been deployed."
 
-	        withEnv(["PATH+PYTHON=/path/to/python/bin"]) {
-                sh 'python get-pip.py --user'
-            	sh 'python -m pip install Flask'
-        	}
+                // Wait for the application to start
+                sleep 5
 
-        	// Start Flask Application in the Background
-	        sh "nohup python ${deployDir}/web.py > /dev/null 2>&1 &"
+                // Send a POST request to /shutdown to stop the Flask application gracefully
+                sh """
+                    python - <<EOF
+                    import requests
+                    requests.post("http://127.0.0.1/shutdown")
+                    EOF
+                """
 
-	        echo "Flask web application has been deployed."
-
-        	// Wait for the application to start
-	        sleep 5
-
-        	// Send a POST request to /shutdown to stop the Flask application gracefully
-        	sh """
-            		python - <<EOF
-            		import requests
-            		requests.post("http://127.0.0.1/shutdown")
-            		EOF
-        	"""
-
-        echo "Flask web application has been stopped."
-		}
-	}
+                echo "Flask web application has been stopped."
+            }
+        }
     }
 }
-
