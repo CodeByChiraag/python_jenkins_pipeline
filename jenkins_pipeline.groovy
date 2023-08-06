d65f68efe35fbe1e75df5b98d2b405100e4cf545
Chiraag Sharma <work.chiraagsharma@gmail.com>
2023-08-07 01:53:40 +0530

    Solved interpreter issue

--- jenkins_pipeline.groovy
+++ jenkins_pipeline.groovy
@@ -39,12 +39,12 @@ pipeline {
                 sleep 10
 
                 // Send a POST request to /shutdown to stop the Flask application gracefully
-                sh """
-                python3 <<EOF &&
-                import requests &&
+                sh '''
+                python3 - <<EOF 
+                import requests 
                 requests.post("http://127.0.0.1/shutdown")
                 EOF
-                """
+                '''
 
                 echo "Flask web application has been stopped."
             }
