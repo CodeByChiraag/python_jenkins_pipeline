26af2859ff35683eda354ad1c7fa84329d153659
Chiraag Sharma <work.chiraagsharma@gmail.com>
2023-08-07 00:35:11 +0530

    solve workspace error

--- jenkins_pipeline.groovy
+++ jenkins_pipeline.groovy
@@ -15,35 +15,40 @@ pipeline {
         }
         
         stage('Deploy') {
-            steps {
-                timestamps {
-                    echo "Deploying the Flask web application..."
-                    
-                    // Application Installation and Startup
-                    sh 'mkdir -p /web'
-                    sh 'curl -O https://bootstrap.pypa.io/get-pip.py'
-                    sh 'python3 get-pip.py --user'
-                    sh 'python3 -m pip install Flask'
-                    
-                    // Start Flask Application in the Background
-                    sh 'nohup python3 web.py > /dev/null 2>&1 &'
-                    
-                    echo "Flask web application has been deployed."
-                    
-                    // Wait for the application to start
-                    sleep 5
-                    
-                    // Send a POST request to /shutdown to stop the Flask application gracefully
-                    sh """
-                        python3 - <<EOF
-                        import requests
-                        requests.post("http://127.0.0.1/shutdown")
-                        EOF
-                    """
-                    
-                    echo "Flask web application has been stopped."
-                }
-            }
-        }
+    	   steps {
+        	echo "Deploying the Flask web application..."
+
+        	// Use the Jenkins workspace as the deployment directory
+        	def deployDir = "$WORKSPACE/web"
+        	sh "mkdir -p ${deployDir}"
+
+       		// Application Installation and Startup
+        	sh 'curl -O https://bootstrap.pypa.io/get-pip.py'
+
+	        withEnv(["PATH+PYTHON=/path/to/python/bin"]) {
+                sh 'python get-pip.py --user'
+            	sh 'python -m pip install Flask'
+        	}
+
+        	// Start Flask Application in the Background
+	        sh "nohup python ${deployDir}/web.py > /dev/null 2>&1 &"
+
+	        echo "Flask web application has been deployed."
+
+        	// Wait for the application to start
+	        sleep 5
+
+        	// Send a POST request to /shutdown to stop the Flask application gracefully
+        	sh """
+            		python - <<EOF
+            		import requests
+            		requests.post("http://127.0.0.1/shutdown")
+            		EOF
+        	"""
+
+        echo "Flask web application has been stopped."
+		}
+	}
     }
 }
+
